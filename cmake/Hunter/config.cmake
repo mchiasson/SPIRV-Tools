061718b814008048bb989c70405863b9abb772ce
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2019-05-08 21:56:14 -0400

    latest

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,6 +1,7 @@
 hunter_config(SPIRV-Headers
-    VERSION v1.3.7-p0
-    URL https://github.com/mchiasson/SPIRV-Headers/archive/v1.3.7-p0.tar.gz
-    SHA1 989a15bd4880508513d234bb9da17fde202b89f8
+    VERSION v1.4.1-p0
+    URL https://github.com/mchiasson/SPIRV-Headers/archive/v1.4.1-p0.tar.gz
+    SHA1 942ba385d1f71da73dab5f5467b53b770f954ad5
     CMAKE_ARGS SPIRV_HEADERS_ENABLE_EXAMPLES=OFF
 )
+
